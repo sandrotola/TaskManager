65da54cef9dffead3d6abae8344f3f077a5dbb27
Sandro Tola <sandro.tola@overapp.it>
2024-02-07 12:13:03 +0100

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Sandro Tola on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
